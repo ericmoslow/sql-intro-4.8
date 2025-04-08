d07b4e3e937c9a93b2b1514ae0dc8bcfe670d94f
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-04-08 15:59:38 +0000

    Edits to lab 3

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,11 +1,11 @@
 -- In the modern era (1960-present), how many regular season games did the best team win each season?
 -- Challenge: Try to include the team's name in the results. Is it accurate?
-SELECT year, name, max(wins) FROM teams
+SELECT year, max(wins) FROM teams
 WHERE year >= 1960
 GROUP BY year
 ORDER BY wins DESC;
 
-SELECT COUNT (*) FROM(SELECT year, name, max(wins) FROM teams
+SELECT COUNT (*) FROM(SELECT year, max(wins) FROM teams
 WHERE year >= 1960
 GROUP BY year
 ORDER BY wins DESC);
